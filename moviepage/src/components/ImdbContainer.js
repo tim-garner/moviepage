bf89e96478fe055a28a8cdb3c7c358d90958a33a
tim-garner <timothy.garner602@gmail.com>
2019-09-01 14:41:20 -0400

    created movie search and api search

--- moviepage/src/components/ImdbContainer.js
+++ moviepage/src/components/ImdbContainer.js
@@ -14,3 +14,12 @@ class ImdbContainer extends Component {
     }
 };
 
+componentDidMount() {
+    this.searchMovies("Tommy Boy");
+}
+
+searchMovies = query => {
+    API.search(query)
+    .then(res => this.setState({ result: res.data}))
+    .catch(err => console.log(err));
+};
