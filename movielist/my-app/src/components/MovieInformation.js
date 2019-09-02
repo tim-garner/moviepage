322419fffd1cf720f062eb965199ae977ac01bec
tim-garner <timothy.garner602@gmail.com>
2019-09-02 15:07:15 -0400

    updated spelling errors

--- movielist/my-app/src/components/MovieInformation.js
+++ movielist/my-app/src/components/MovieInformation.js
@@ -6,7 +6,7 @@ function MovieInformation(props) {
             <img alt={props.title} className="img-fluid" src={props.src} style={{ margin: "12px auto"}} />
             <h4>Directors: {props.director} </h4>
             <h4>Genre: {props.genre}</h4>
-            <h4>REleased: {props.released}</h4>
+            <h4>Released: {props.released}</h4>
             </div>
 
 
